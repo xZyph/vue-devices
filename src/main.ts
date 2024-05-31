42d320357ee4efdb90d61c9645d3510ecda1be28
Marius Mikelsen <nerdesatan@gmail.com>
2024-05-31 13:22:44 +0200

    Added import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { App } from 'vue'
 import VueDevice from './components/VueDevice.vue'
+import 'devices.css/dist/devices.min.css'
 
 const install = (app: App): void => {
   app.component('VueDevice', VueDevice)
